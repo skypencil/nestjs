fbb80bad87d369b64f569fd5a0abdecffee7df3a
SalimBaktash <sstechnologyindtec@gmail.com>
2024-01-30 13:35:33 +0430

    Removing the api folder from app folder

--- front/app/api/[author]/route.ts
+++
@@ -1,8 +0,0 @@
-import { Params } from "@/app/components/Params"
-import { data } from "../data"
-
-export async function GET(request:Request, {params}:{params: Params}) {
-    const todo = data.filter(item => item.author === params.author)
-
-    return Response.json(todo)
-}
